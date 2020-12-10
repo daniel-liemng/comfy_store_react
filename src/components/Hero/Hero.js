ee455b0f4640be19f7f26d17ef21a3a3bdbece8b
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2020-12-10 18:42:17 -0500

    Home - Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import HeroWrapper from "./HeroWrapper";
+import heroBcg from "../../assets/hero-bcg.jpeg";
+import heroBcg2 from "../../assets/hero-bcg-2.jpeg";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <HeroWrapper className='section-center'>
+      <article className='content'>
+        <h1>
+          design your <br />
+          confort zone
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi autem
+          obcaecati explicabo accusamus ratione porro ea quasi at aspernatur ad.
+        </p>
+        <Link to='/products' className='btn hero-btn'>
+          shop now
+        </Link>
+      </article>
+
+      <article className='img-container'>
+        <img src={heroBcg} alt='nice table' className='main-img' />
+        <img src={heroBcg2} alt='person working' className='accent-img' />
+      </article>
+    </HeroWrapper>
+  );
 };
 
 export default Hero;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import { FeaturedProducts, Hero, Services, Contact } from "../../components";
+
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <Hero />
+      <FeaturedProducts />
+      <Services />
+      <Contact />
+    </main>
+  );
 };
 
 export default HomePage;
