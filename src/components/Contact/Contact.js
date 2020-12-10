d1adc8af8bc15c1d6984b59ee1b6c08f2e0f1c52
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2020-12-10 18:55:38 -0500

    Service - Contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,33 @@
 import React from "react";
 
+import ContactWrapper from "./ContactWrapper";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <ContactWrapper>
+      <div className='section-center'>
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            veniam repudiandae vel ab id, fuga praesentium nobis natus ipsam
+            vero?
+          </p>
+          <form className='contact-form'>
+            <input
+              type='email'
+              className='form-input'
+              placeholder='enter email'
+              name='_replyto'
+            />
+            <button type='button' className='submit-btn'>
+              subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </ContactWrapper>
+  );
 };
 
 export default Contact;
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,7 +1,36 @@
 import React from "react";
 
+import ServicesWrapper from "./ServicesWrapper";
+import { services } from "../../utils/constants";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <ServicesWrapper>
+      <div className='section-center'>
+        <article className='header'>
+          <h3>
+            custom furniture <br />
+            built only for you
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint
+            perferendis libero eveniet corrupti illum deleniti vel laboriosam
+            eligendi voluptatem at?
+          </p>
+        </article>
+
+        <div className='services-center'>
+          {services.map(({ id, icon, title, text }) => (
+            <article className='service' key={id}>
+              <span className='icon'>{icon}</span>
+              <h4>{title}</h4>
+              <p>{text}</p>
+            </article>
+          ))}
+        </div>
+      </div>
+    </ServicesWrapper>
+  );
 };
 
 export default Services;
