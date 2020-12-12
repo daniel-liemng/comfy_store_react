ea87007673a44780534cc33889c57b1751e57e8f
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2020-12-11 22:47:21 -0500

    Stars - Array

--- src/components/Stars/Stars.js
+++ src/components/Stars/Stars.js
@@ -4,62 +4,88 @@ import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 import StarsWrapper from "./StarsWrapper";
 
 const Stars = ({ stars, reviews }) => {
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    const decimalNumber = index + 0.5;
+    return (
+      <span key={index}>
+        {stars >= index + 1 ? (
+          <BsStarFill />
+        ) : stars >= decimalNumber ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    );
+  });
+
+  console.log(tempStars);
+
+  ///// ARRAY - DYNAMIC /////
   return (
     <StarsWrapper>
-      <div className='stars'>
-        <span>
-          {stars >= 1 ? (
-            <BsStarFill />
-          ) : stars >= 0.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-
-        <span>
-          {stars >= 2 ? (
-            <BsStarFill />
-          ) : stars >= 1.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-
-        <span>
-          {stars >= 3 ? (
-            <BsStarFill />
-          ) : stars >= 2.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-
-        <span>
-          {stars >= 4 ? (
-            <BsStarFill />
-          ) : stars >= 3.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-
-        <span>
-          {stars === 5 ? (
-            <BsStarFill />
-          ) : stars >= 4.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-      </div>
+      <div className='stars'>{tempStars}</div>
       <p className='reviews'>({reviews} customer reviews)</p>
     </StarsWrapper>
   );
+
+  ///// MANUAL /////
+  // return (
+  //   <StarsWrapper>
+  //     <div className='stars'>
+  //       <span>
+  //         {stars >= 1 ? (
+  //           <BsStarFill />
+  //         ) : stars >= 0.5 ? (
+  //           <BsStarHalf />
+  //         ) : (
+  //           <BsStar />
+  //         )}
+  //       </span>
+
+  //       <span>
+  //         {stars >= 2 ? (
+  //           <BsStarFill />
+  //         ) : stars >= 1.5 ? (
+  //           <BsStarHalf />
+  //         ) : (
+  //           <BsStar />
+  //         )}
+  //       </span>
+
+  //       <span>
+  //         {stars >= 3 ? (
+  //           <BsStarFill />
+  //         ) : stars >= 2.5 ? (
+  //           <BsStarHalf />
+  //         ) : (
+  //           <BsStar />
+  //         )}
+  //       </span>
+
+  //       <span>
+  //         {stars >= 4 ? (
+  //           <BsStarFill />
+  //         ) : stars >= 3.5 ? (
+  //           <BsStarHalf />
+  //         ) : (
+  //           <BsStar />
+  //         )}
+  //       </span>
+
+  //       <span>
+  //         {stars === 5 ? (
+  //           <BsStarFill />
+  //         ) : stars >= 4.5 ? (
+  //           <BsStarHalf />
+  //         ) : (
+  //           <BsStar />
+  //         )}
+  //       </span>
+  //     </div>
+  //     <p className='reviews'>({reviews} customer reviews)</p>
+  //   </StarsWrapper>
+  // );
 };
 
 export default Stars;
